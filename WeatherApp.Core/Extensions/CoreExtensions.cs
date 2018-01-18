19a6834bd977e98ef9aa37e849ea777a7421c294
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@hotmail.com>
2018-01-18 01:45:42 +0100

    ApiConstants and CoreExtensions has been created

---
+++ WeatherApp.Core/Extensions/CoreExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Extensions
+{
+    public static class CoreExtensions
+    {
+
+        public static Task ToTaskRun(this Task me)
+        {
+            return Task.Run(async () => { await me; });
+        }
+
+    }
+}
